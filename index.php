75ed895a90b288a328da1bad3094e4eeaa86cbf3
Norbert Peters <norbert@nextid.nl>
2013-05-18 15:54:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
-﻿<?
+<?
 @apache_setenv('no-gzip', 1);
-
 include("xlsx.php");
 
+/*settings*/
 $filetplpath = "test.xlsx";
 $fileexportname = "testexport.xlsx";
+$csvpath1 = "test.csv";
+$csvtosheet = "csvdata";
+$csvtocol = "A";
+$csvtorow = "2";
+$csvdelimiter = ",";
+$csvenclosure = '"';
 
+/*create temp xlsx file*/
 $tmppath=tempnam('php://temp', 'xl_');
 copy($filetplpath,$tmppath);
 
+/*open tempfile, import the csv file, and set pivots to refresh*/
 $xl=new xlsx($tmppath);
-$xl->importcsv("csvdata","test.csv",",",'"','A','2');
+$xl->importcsv($csvtosheet,$csvpath1,$csvdelimiter,$csvenclosure,$csvtocol,$csvtorow);
 $xl->refreshPivotsOnOpen ();
 $xl->close();
 
+/*output tempfile*/
 header('Content-Disposition: attachment;filename="'.$fileexportname.'"');
 ob_clean();   
 readfile($tmppath);
